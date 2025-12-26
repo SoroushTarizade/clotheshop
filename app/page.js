7d80cc6c319ee24c13e8676224c06ae9473616bc
soorushtarizade <soroushtarizadeh7139@gmail.com>
2025-12-26 15:40:14 +0330

    header is completed

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Navbar from "@/components/navbar/Navbar";
 import Header from "@/components/header/Header";
 export default function Home() {
   return (
     <>
+        <Navbar></Navbar>
         <Header></Header>
     </>
   );
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,56 +1,27 @@
-import Link from 'next/link';
-import { React } from 'react';
-import { IoIosSearch } from "react-icons/io";
+import React from 'react';
+import { IoIosArrowRoundForward } from "react-icons/io";
 const Header = () => {
     return (
         <>
-        <div className='flex items-center justify-around h-[62px] pl text-lg'>
-            <div className='flex items-center justify-between gap-10 text-[#FDF6F6] '>
-            <div className="flex items-center gap-1">
-                <div className="border-3xl w-10">
-                    <img className="rounded-full" src="./images/logo.jpg" alt="logo-img" />
+            <div className='flex items-center w-[1200px] m-auto justify-between mt-[-450px] px-10'>
+                <div className='text-[#FDF6F6]'>
+                    <h1 className='font-700 text-[64px] mb-10'>
+                        How to Rent?
+                    </h1>
+                    <p className='w-[613px] h-[66px] text-left text-justify text-balance mb-5'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
+                    <button className='bg-[#F2B138] w-[140px] h-[38px] rounded-[28px] flex items-center gap-3 px-[22px] py-[10px] text-[15px] font-500 cursor-pointer hover:font-bold'>
+                        See More
+                        <span>
+                            <IoIosArrowRoundForward className='w-[14px]'></IoIosArrowRoundForward>
+                        </span>
+                    </button>
                 </div>
                 <div>
-                    <p className="">Clothe Shop</p>
+                    <img src="./images/header-img.jpg" alt="header-img" className='w-[342px] h-[532px] rounded-t-[171px]'/>
                 </div>
             </div>
-            <div>
-                <ul className="flex gap-5 items-center ">
-                    <li>
-                        <Link href="/" className='hover:font-bold'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href="discover" className='hover:font-bold'>Discover</Link>
-                    </li>
-                    <li>
-                        <Link href="faqs"  className='hover:font-bold'>
-                        FAQs
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="about" className='hover:font-bold'>
-                        About Us
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="connect" className='hover:font-bold'>Connect Us</Link>
-                    </li>
-                </ul>
-            </div>
-            </div>
-            <div className="flex gap-2 items-center ">
-            <div className="">
-                <IoIosSearch className="w-[24px]"></IoIosSearch>
-            </div>
-            <div>
-                <span className='border-l-[.1rem] mr-3 ml-1 border-gray-400 w-[38px]'></span>
-                <button className='bg-[#F2B138] text-[#ffffff] rounded-[25px] h-[33px] w-[5.9rem]'>
-                    <Link href="signin">Sign In</Link>
-                </button>
-            </div>
-            </div>
-        </div>
-        <div className="w-[1138px] h-[611px] bg-[#01727D] mt-[-67px] ml-[-295px] rounded-br-[171px]"></div>
         </>
     );
 }
